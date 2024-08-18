c8338235319307a2dad35df360581b1e3287f22d
Pedro Mattos <112045648+pedromattosg@users.noreply.github.com>
2024-08-18 21:40:09 +0000

    Pedro_Henrique_De_Mattos_Gomes
    Comentários detalhados na implementação e no cabeçalho da classe Jogador

--- include/Jogador.hpp
+++ include/Jogador.hpp
@@ -12,20 +12,22 @@ private:
 
   std::string apelido;
   std::string nome;
-  // <jogo,{int_vitoria, int_derrota, int_empate}>
+  // map<nome_jogo, {int_vitorias, int_derrotas, int empates}>. Container para armazenar as estatísticas do jogador. Consiste em um map no qual a chave é o nome jogo e o valor é um array com 3 posições. A primeira posição armazena o número de vitórias, a segunda o de derrotas e a terceira o de empates.
   std::map<std::string, std::array<int, 3>> estatisticas;
 
 public:
  Jogador(const std::string& apelido, const std::string& nome);
  Jogador() = default;  // Construtor padrão para leitura de arquivos
  ~Jogador() = default;
-  //Jogador(std::string, std::string);
-  //~Jogador();
   std::string getApelido() const;
   std::string getNome() const;
+  // Imprime as estatísticas do jogador
   void listarEstatisticas() const;
+  // Adiciona estatísticas ao jogador
   void setEstatistica(std::string, int);
+  // Salva os dados do jogador em um arquivo
   void salvar(std::ofstream& out) const;
+  // Carrega os dados do jogador de um arquivo
   void carregar(std::ifstream& in);
   
 };
--- src/Jogador.cpp
+++ src/Jogador.cpp
@@ -14,39 +14,72 @@ std::string Jogador::getNome() const {
     return this->nome;
 }
 
+// Imprime as estatísticas do jogador
 void Jogador::listarEstatisticas() const {
     std::cout << this->apelido << " " << this->nome << std::endl;
+    // iterador para percorrer o map
+    // jogo.first -> nome do jogo
+    // jogo.second -> array com 3 posições ([0]=num_vitorias [1]=num_derrotas [2]=num_empates)
     for (const auto& jogo : this->estatisticas) {
       std::cout << jogo.first << " - V: " << jogo.second[0] << " D: " << jogo.second[1] << " E: " << jogo.second[2] << std::endl;
     }
 }
 
+// Adiciona estatísticas ao jogador
+// Recebe como parâmetros o (nome do jogo) e o (resultado), sendo:
+// 0: vitória
+// 1: derrota
+// 2: empate
 void Jogador::setEstatistica(std::string jogo, int resultado) {
+  // Verifica se o jogo já existe no map
   if (this->estatisticas.find(jogo) != this->estatisticas.end()) {
+    // Incrementa o numero de vitorias/derrotas/empates ao jogo
     this->estatisticas[jogo][resultado]++;
   }
+  // Caso o jogo não exista no map
   else {
+    // Adiciona uma nova chave no map com o nome do jogo
+    // Adicionar o array com as vitorias/derrotas/empates zeradas
     this->estatisticas[jogo] = {0, 0, 0};
+    // Incrementa o numero de vitoria/derrota/empate ao jogo
     this->estatisticas[jogo][resultado]++;
   }
 }
 
+// Salva os dados do jogador em um arquivo
+// Recebe como parâmetro o fluxo de saída do arquivo a ser salvo
 void Jogador::salvar(std::ofstream& out) const {
+  // Salva o apelido na primeira linha
+  // Salva o nome na segunda linha
   out << apelido << '\n' << nome << '\n';
+  // Iterador para o map de estatísticas
+  // jogo.first -> nome do jogo
+  // jogo.second -> array com 3 posições ([0]=num_vitorias [1]=num_derrotas [2]=num_empates)
   for (const auto& jogo : this->estatisticas) {
+      // Salva o nome do jogo seguido do número de vitórias, derrotas e empates
       out << jogo.first << ' ' << jogo.second[0] << ' ' << jogo.second[1] << ' ' << jogo.second[2] << '\n';
   }
+  // Indica o final de dados "úteis" no arquivo
   out << "---\n";
 }
 
+// Carrega os dados do jogador de um arquivo
+// Recebe como parâmetro o fluxo de entrada do arquivo a ser carregado
 void Jogador::carregar(std::ifstream& in) {
+  // Lê o apelido na primeira linha
   std::getline(in, this->apelido);
+  // Lê o nome na segunda linha
   std::getline(in, this->nome);
+  // Zera as estatísticas do jogador
   this->estatisticas.clear();
+  // Variáveis auxiliares para leitura das estatísticas do arquivo
   std::string jogo;
   int v, d, e;
+  // Lê o nome do jogo e o número de vitórias, derrotas e empates em cada linha
   while (in >> jogo >> v >> d >> e) {
+      // Salva as estatísticas de cada jogo no map
       this->estatisticas[jogo] = {v, d, e};
   }
+  // Ignora linhas extras que podem constar no arquivo
   in.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
 }
